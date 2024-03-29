4950ae2303154132f33dd58c2775bb1ff8e278a8
arianna choza <unachoza@gmail.com>
2024-03-29 12:38:23 -0400

    content layout html

--- src/App.tsx
+++ src/App.tsx
@@ -5,58 +5,44 @@ const App = () => {
 
   return (
     <>
-     <body>
-  Earth
-
-  {/* <!-- Overview Content Start --> */}
-
-  Third planet from the Sun and the only known planet to harbor life. 
-  About 29.2% of Earth's surface is land with remaining 70.8% is 
-  covered with water. Earth's distance from the Sun, physical properties 
-  and geological history have allowed life to evolve and thrive.
-
-  Source : Wikipedia {/* <!-- URL: https://en.wikipedia.org/wiki/Earth --> */}
-
-  {/* <!-- Overview Content End --> */}
-
-  {/* <!-- Internal Structure Content Start --> */}
-
-  Earth's interior, like that of the other terrestrial planets, is divided 
-  into layers by their chemical or physical (rheological) properties. The 
-  outer layer is a chemically distinct silicate solid crust, which is 
-  underlain by a highly viscous solid mantle.
-
-  Source : Wikipedia {/* <!-- URL: https://en.wikipedia.org/wiki/Earth#Internal_structure -->*/}
-
-  {/* <!-- Internal Structure Content End --> */}
-
-  {/* <!-- Surface Geology Content Start --> */}
-
-  The total surface area of Earth is about 510 million km2. The continental 
-  crust consists of lower density material such as the igneous rocks granite 
-  and andesite. Less common is basalt, a denser volcanic rock that is the 
-  primary constituent of the ocean floors.
-
-   Source : Wikipedia {/*<!-- URL: https://en.wikipedia.org/wiki/Earth#Surface --> */}
-
-  {/* <!-- Surface Geology Content End --> */}
-
-  01 Overview
-  02 Internal Structure
-  03 Surface Geology
-
-  Rotation Time
-  0.99 Days
-
-  Revolution Time
-  365.26 Days
-
-  Radius
-  6,371 KM
-
-  Average Temp.
-  16°c
-</body>
+      <nav></nav>
+      <main>
+        <img src="" alt="" />
+        <section>
+          <h1> Earth</h1>
+          <div className="content">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos quo ipsum ducimus maxime, incidunt quisquam officiis, nulla nihil pariatur alias laudantium itaque expedita veritatis unde neque ratione quidem! Placeat, sint.
+          </div>
+          <div className="source">
+            <span>link</span>
+          </div>
+          <div className="content-link-list">
+            <h3><span></span>Overview</h3>
+            <h3><span></span>Internal Structure</h3>
+            <h3><span></span>Surface Geology</h3>
+          </div>
+        </section>
+      </main>
+      <section>
+        <ul>
+          <li>
+            <span></span>
+            <div className="fact"></div>
+          </li>
+          <li>
+              <span></span>
+            <div className="fact"></div>
+          </li>
+          <li>
+              <span></span>
+            <div className="fact"></div>
+          </li>
+          <li>
+              <span></span>
+            <div className="fact"></div>
+          </li>
+        </ul>
+      </section>
     </>
   )
 }
